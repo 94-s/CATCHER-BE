0dab98835723d9edfaebbf18c89e9263a729bd28
SunghyunKim9744 <dqq9744@naver.com>
2022-07-30 14:25:25 +0900

    Init: Spring Project

---
+++ src/main/java/com/project/catcher/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.project.catcher.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "hiasd";
+    }
+}
